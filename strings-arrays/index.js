ee5f820e0db76cf78525c3fe682175b1090402d6
evai-git <evair-23@bol.com.br>
2021-06-16 17:14:39 -0300

    Exercicio de strings e arrays

---
+++ strings-arrays/index.js
@@ -0,0 +1,31 @@
+
+// 1 - a = undefined, b = null , c = 11 , d = 3 , e = (3, 19, 5,6,7,8,9,10,11,12,13) , f = 9
+
+// 2 - subir num onibus em mirrocos , 28.
+
+// const nomeDoUsuario = prompt("Qual o seu nome?")
+// const emailDoUsuario = prompt("Qual o seu e-mail?")
+// console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso.")
+// console.log("Seja bem vindo", nomeDoUsuario)
+
+
+const minhasComidasPrediletas = ["strogonof","hamburguer","salmao","arroz","feijao"]
+console.log(minhasComidasPrediletas)
+
+console.log('Essas são minhas comidas favoritas:',
+${minhasComidasPrediletas[0]}
+${minhasComidasPrediletas[1]}
+${minhasComidasPrediletas[2]}
+${minhasComidasPrediletas[3]}
+${minhasComidasPrediletas[4]})
+
+const userFood = prompt('qual é sua comida favorita?')
+minhasComidasPrediletas[1] = userFood
+console.log(minhasComidasPrediletas)
+
+
+
+
+
+
+
