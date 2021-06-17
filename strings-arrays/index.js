f06dff1a9e88241c148051139eda21d35dbb0c60
evai-git <evair-23@bol.com.br>
2021-06-17 16:33:38 -0300

    funcoes

--- strings-arrays/index.js
+++ strings-arrays/index.js
@@ -1,7 +1,7 @@
 
 // 1 - a = undefined, b = null , c = 11 , d = 3 , e = (3, 19, 5,6,7,8,9,10,11,12,13) , f = 9
 
-// 2 - subir num onibus em mirrocos , 28.
+// 2 - SUBA NUM ONABUS EM MIRROCOS , 27.
 
 // const nomeDoUsuario = prompt("Qual o seu nome?")
 // const emailDoUsuario = prompt("Qual o seu e-mail?")
@@ -9,23 +9,32 @@
 // console.log("Seja bem vindo", nomeDoUsuario)
 
 
-const minhasComidasPrediletas = ["strogonof","hamburguer","salmao","arroz","feijao"]
-console.log(minhasComidasPrediletas)
+ //const minhasComidasPrediletas = ["strogonof","hamburguer","salmao","arroz","feijao"]
+// console.log(minhasComidasPrediletas)
 
-console.log('Essas são minhas comidas favoritas:',
-${minhasComidasPrediletas[0]}
-${minhasComidasPrediletas[1]}
-${minhasComidasPrediletas[2]}
-${minhasComidasPrediletas[3]}
-${minhasComidasPrediletas[4]})
+// console.log('Essas são minhas comidas favoritas:')
+// console.log(minhasComidasPrediletas[0])
+// console.log(minhasComidasPrediletas[1])
+// console.log(minhasComidasPrediletas[2])
+// console.log(minhasComidasPrediletas[3])
+// console.log(minhasComidasPrediletas[4])
 
-const userFood = prompt('qual é sua comida favorita?')
-minhasComidasPrediletas[1] = userFood
-console.log(minhasComidasPrediletas)
-
-
+// const novaComida = prompt('Diga uma comida que você gosta?')
+// minhasComidasPrediletas[1] = novaComida
+// console.log(minhasComidasPrediletas)
 
 
+// const listaDeTarefas = []
+// const primeiraTarefa = prompt('digite a primeira tarefa')
+// const segundaTarefa = prompt('digite a segunda tarefa')
+// const terceiraTarefa = prompt('digite a terceira tarefa')
 
+// listaDeTarefas.push(primeiraTarefa)
+// listaDeTarefas.push(segundaTarefa)
+// listaDeTarefas.push(terceiraTarefa)
 
+// console.log(listaDeTarefas)
 
+// const tarefaRealizada = prompt('digite o indice da tarefa que realizou: 0, 1 ou 2')
+// listaDeTarefas.splice(Number(tarefaRealizada), 1)
+// console.log(listaDeTarefas)
