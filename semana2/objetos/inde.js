bc78a5d62ffe45ff687ab74dddd8be0ee0cb73ed
evai-git <evair-23@bol.com.br>
2021-06-22 16:48:27 -0300

    Exercicio condicionais

--- semana2/objetos/inde.js
+++ semana2/objetos/inde.js
@@ -1,4 +1,9 @@
-// 1 - a - vai ser impresso "Matheus nachtergaele", "Virginia Cavendish", "canal Brasil", 19h.
+
+// Exercicio de interpretação de texto
+
+// 1 - a - vai ser impresso "Matheus nachtergaele", "Virginia Cavendish", "canal Globo", 14h.
+
+
 
 //  2 - a - cahorro:  "Juca", idade: 3, raca: "SRD   
 //          gato: "Juba", idade: 3, raca: "SRD"
@@ -14,11 +19,95 @@
  
 // exercicio 1 escrita de codigos:
 
-const pessoa = {
-    nome: "lucas",
-    apelidos: ["motoboy", "tafarel", "louco"],
+// const pessoa = {
+//     nome: "lucas",
+//     apelidos: ["motoboy", "tafarel", "louco"],
+// }
+
+// function imprimePessoa(objeto){
+//     console.log(`Eu sou ${objeto.nome}, mas pode me chamar de: ${objeto.apelidos[0]}, ${objeto.apelidos[1]} ou ${objeto.apelidos[2]}`)
+
+// }
+
+//imprimePessoa(pessoa)
+
+// b -
+
+
+// const novaPessoa = {...pessoa, apelidos:[
+//     "motoboy", "tafarel", "louco"
+// ]}
+
+
+// imprimePessoa(novaPessoa)
+
+
+
+// Exercicio 2 letra a:
+
+
+// const pessoa = {
+//     nome: "paulo",
+//     idade: 25,
+//     profissao: "arquiteto"
+// }
+
+// function imprimeInfos(person){
+//     return [person.nome, person.nome.length, person.idade, person.profissao, person.profissao.length]
+// }
+
+// console.log(imprimeInfos(pessoa))
+
+
+// exercicio 3 
+
+// a :
+
+const carrinho = []
+
+// b:
+
+const frutas1 = {
+    nome: "abacaxi",
+    disponibilidade: true
+
 }
-console.log(`Eu sou o Lucas , mas pode me chamar de: ${apelidos[0].nome}, ${apelidos[1].nome}, ${apelidos[2]}`)
+
+const frutas2 = {
+    nome: "uva",
+    disponibilidade: true
+
+}
+
+const frutas3 = {
+    nome: "pera",
+    disponibilidade: true
+
+}
+
+const frutas4 = {
+    nome: "laranja",
+    disponibilidade: true
+
+}
+
+// c -
+
+function adicionaFrutaNoCarrinho(fruta) {
+carrinho.push(fruta)
+}
+
+adicionaFrutaNoCarrinho(abacaxi)
+adicionaFrutaNoCarrinho(uva)
+adicionaFrutaNoCarrinho(pera)
+adicionaFrutaNoCarrinho(laranja)
+
+
+console.log(carrinho)
+
+
+
+
 
 
 
