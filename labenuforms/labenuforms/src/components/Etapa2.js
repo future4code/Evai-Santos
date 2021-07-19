5127bf6627312be3f896cb7bb33f109406e124df
evai-git <evair-23@bol.com.br>
2021-07-19 17:01:48 -0300

    atividade labeform

---
+++ labenuforms/labenuforms/src/components/Etapa2.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './app.css';
+
+
+
+
+function Etapa2() {
+  return (
+    <div>
+        <h1>ETAPA 2- INFORMAÇÕES DO ENSINO SUPERIOR</h1>
+
+        <imput><h2>Qual o curso?</h2></imput>
+
+        <imput><h2>Qual unidade de ensino?</h2></imput>
+
+
+
+    
+
+
+
+
+     
+    </div>
+  );
+}
+
+export default Etapa2;
