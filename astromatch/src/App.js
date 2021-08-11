8579fe3832b3d6cfc5f3f9d030a5814df4020071
evai-git <evair-23@bol.com.br>
2021-08-11 16:54:32 -0300

    astrodev modo inicial

---
+++ astromatch/src/App.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { HomePage } from "./pages/HomePage/HomePage"
+import { MatchsPage } from "./pages/MatchsPage/MatchsPage"
+
+
+
+
+
+const App = () => {
+  return(
+    <div>
+      Astromatch
+      <HomePage />
+      <MatchsPage />
+    </div>
+  )
+}
+
+export default App
